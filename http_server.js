f6c758971971b8094ed18a0f3f8ea63379818401
Róbert Mysza <xmysza00@stud.fit.vutbr.cz>
2021-10-26 01:05:48 +0200

    Serving web browsers

--- http_server.js
+++ http_server.js
@@ -1,20 +1,45 @@
 const fs = require('fs');
+const path = require("path");
 const http = require('http');
 
-const dir = 'data/'
+const port = 8080;
+const dir = 'data/';
+let data = '';
+
+const getMostRecentFile = (dir) => {
+  const files = orderReccentFiles(dir);
+  return files.length ? files[0] : undefined;
+};
+
+const orderReccentFiles = (dir) => {
+  return fs.readdirSync(dir)
+    .filter((file) => fs.lstatSync(path.join(dir, file)).isFile())
+    .map((file) => ({ file, mtime: fs.lstatSync(path.join(dir, file)).mtime }))
+    .sort((a, b) => b.mtime.getTime() - a.mtime.getTime());
+};
 
 const requestListener = function (req, res) {
-  // receive chunks of data
-  let data = '';
-  req.on('data', chunk => {
-    data += chunk;
-  })
-  req.on('end', () => {
-    // send response
-    res.writeHead(200);
+  req.on('error', (err) => {
+    console.error(err);
+    res.statusCode = 400;
     res.end();
+  });
+  res.on('error', (err) => {
+    console.error(err);
+  });
 
-    if (req.headers["user-agent"] == "Arduino/1.0") {
+  if (req.method === 'POST' && req.url === '/data') {
+    // && req.headers["user-agent"] == "Arduino/1.0") {
+    // receive chunks of data
+    data = '';
+    req.on('data', chunk => {
+      data += chunk;
+    })
+    req.on('end', () => {
+      // send response
+      res.writeHead(200);
+      res.end();
+
       // get csv data and received date as last line
       let csv_data = '';
       let file_date = 'output';
@@ -38,8 +63,40 @@ const requestListener = function (req, res) {
       console.log(csv_data);
       console.log();
       console.log(Date());
+    })
+  }
+  else if (req.method === 'GET' && req.url === '/') {
+    let lastFile = getMostRecentFile(dir);
+    if (lastFile !== undefined) {
+      fs.readFile(dir + lastFile.file, (err, data) => {
+        if (!err) {
+          res.writeHead(200);
+          res.end(lastFile.mtime + '\n\n' + data);
+        }
+      })
     }
-  })
+    else {
+      res.writeHead(200);
+      res.end("No file");
+    }
+  }
+  else if (req.method === 'GET' && req.url === '/download') {
+    let lastFile = getMostRecentFile(dir);
+    if (lastFile !== undefined) {
+      // create read steam for the pdf
+      const rs = fs.createReadStream(dir + lastFile.file);
+      res.setHeader("Content-Disposition", "attachment; filename=" + lastFile.file);
+      rs.pipe(res);
+    }
+    else {
+      res.writeHead(200);
+      res.end("No file");
+    }
+  }
+  else {
+    res.statusCode = 404;
+    res.end("404: Not found");
+  }
 
   // print http orignal message like curl
   // request.method, URL and httpVersion
@@ -55,4 +112,4 @@ const requestListener = function (req, res) {
 }
 
 const server = http.createServer(requestListener);
-server.listen(8080);
+server.listen(port);
